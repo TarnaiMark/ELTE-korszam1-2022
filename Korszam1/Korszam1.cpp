e94c27c14660c35555a89235e5870c46b62b15e5
Tarnai Márk <75064169+TarnaiMark@users.noreply.github.com>
2023-04-02 16:29:33 +0200

    Project generation

---
+++ Korszam1/Korszam1.cpp
@@ -0,0 +1,12 @@
+﻿// Korszam1.cpp : Defines the entry point for the application.
+//
+
+#include "Korszam1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
