e235ca419cb8232952f4111f8e996280e1e2b07d
Tarnai Márk <75064169+TarnaiMark@users.noreply.github.com>
2023-04-02 15:39:52 +0000

    házi

--- Korszam1/Korszam1.cpp
+++ Korszam1/Korszam1.cpp
@@ -5,8 +5,24 @@
 
 using namespace std;
 
+
+ template<typename K> K sq(K x){ return x*x; }
+ template<typename H> H lin(H x){ return 2*x; }
+
+
+template< typename T, typename F, typename H>
+T integrate(F f, H g ,int n, T x0, T x1)
+{
+	double dx = (x1 - x0) / n, sum = 0.0;
+	for (int i = 1; i <= n - 1; i++)
+	{
+		sum += f(x0 + i * dx);
+	}
+	return dx / 2.0 * (f(x0) + g(sum)  + f(x1));
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	double res = integrate( sq<double>, lin<double> ,1000, 0.0, 1.0);
+	cout << res << endl;
 }
---
+++ build/CMakeFiles/Korszam1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Korszam1.dir/Korszam1.cpp.o"
+  "Korszam1"
+  "Korszam1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Korszam1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
