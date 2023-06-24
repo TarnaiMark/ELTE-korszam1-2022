150a489a78ffa182df823246752fb87f4e2f258a
Tarnai Márk <75064169+TarnaiMark@users.noreply.github.com>
2023-06-24 13:06:32 +0000

    3. kis beadandó feladat

--- Korszam1/Korszam1.cpp
+++ Korszam1/Korszam1.cpp
@@ -3,25 +3,7 @@
 
 #include "Korszam1.h"
 
-
 using namespace std;
-
-
- template<typename K> K sq(K x){ return x*x; }
- template<typename H> H lin(H x){ return 2*x; }
-
-
-template< typename T, typename F, typename H>
-T integrate(F f, H g ,int n, T x0, T x1)
-{
-	double dx = (x1 - x0) / n, sum = 0.0;
-	for (int i = 1; i <= n - 1; i++)
-	{
-		sum += f(x0 + i * dx);
-	}
-	return dx / 2.0 * (f(x0) + g(sum)  + f(x1));
-}
-
 int main()
 {
 	double res = integrate( sq<double>, lin<double> ,1000, 0.0, 1.0);
@@ -30,5 +12,22 @@ int main()
 	Vector2d u = { 3, 4 };
 	double  w = v.dot( u);
 	std::cout<<"The sum of the two vectors is:\n"<< w <<std::endl;
+
+	Matrix<double> A(2, 2);
+	A(0, 0) = 1;
+	A(0, 1) = 2;
+	A(1, 0) = 3;
+	A(1, 1) = 4;
+
+	Matrix<double> B(2, 2);
+	B(0, 0) = 1;
+	B(0, 1) = 2;
+	B(1, 0) = 3;
+	B(1, 1) = 4;
+
+	Matrix<double> C = A * 6;
+
+	std::cout << "The matrix is:\n" << C << std::endl;
+
 	
 }
--- Korszam1/Korszam1.h
+++ Korszam1/Korszam1.h
@@ -5,6 +5,27 @@
 
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <stdexcept>
 #include "vector2.h"
+#include "matrix.h"
+using namespace std;
+
+
+template<typename K> K sq(K x){ return x*x; }
+template<typename H> H lin(H x){ return 2*x; }
+
+template< typename T, typename F, typename H>
+T integrate(F f, H g ,int n, T x0, T x1)
+{
+	double dx = (x1 - x0) / n, sum = 0.0;
+	for (int i = 1; i <= n - 1; i++)
+	{
+		sum += f(x0 + i * dx);
+	}
+	return dx / 2.0 * (f(x0) + g(sum)  + f(x1));
+
+	
+}
 
 // TODO: Reference additional headers your program requires here.
